a96444e7c2c2782880fba00d245e9a284cd1a0f8
bunniicat <nina.nny97@gmail.com>
2019-06-01 18:35:40 +0200

    1/06 update

---
+++ Code/scriptNav.js
@@ -0,0 +1,10 @@
+//mobile nav//
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+    console
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  } 
